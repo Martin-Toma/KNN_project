3f2f44250e7ee0ae90d429a9e4156d6226c32f9e
Martin-Toma <martintomasovic3@gmail.com>
2025-04-29 17:13:48 +0200

    working lora finetuning

--- finetuning/finetune_lora.py
+++ finetuning/finetune_lora.py
@@ -67,8 +67,8 @@ SEED_SPLIT = 0
 SEED_TRAIN = 0
 
 #MAX_SEQ_LEN = 128
-TRAIN_BATCH_SIZE = 2 
-EVAL_BATCH_SIZE = 2
+TRAIN_BATCH_SIZE = 1 
+EVAL_BATCH_SIZE = 1
 LEARNING_RATE = 5e-4 
 LR_WARMUP_STEPS = 0
 WEIGHT_DECAY = 0.01
@@ -81,7 +81,7 @@ login(token = ac)
 
 
 # load model and tokenizer
-name = "google/gemma-3-12b-pt" #"mistralai/Mistral-7B-v0.3" #"tiiuae/falcon-7b"
+name = "mistralai/Mistral-7B-v0.3" #"google/gemma-3-12b-pt" #"mistralai/Mistral-7B-v0.3" #"tiiuae/falcon-7b"
 model_pth = SERVER_PTH + "/lora/mpt-7b"
 
 # set attention implementation to "torch"
@@ -134,7 +134,7 @@ model = AutoModelForCausalLM.from_pretrained(
     config=config,  # <--- Pass the modified config here
     #torch_dtype=torch.bfloat16,  # Load model weights in bfloat16
     trust_remote_code=True,
-    device_map="auto",
+    #device_map="auto",
     token=ac
     #force_download=True,
     #resume_download=False
@@ -143,7 +143,17 @@ model = AutoModelForCausalLM.from_pretrained(
 # is not support model.gradient_checkpointing_enable() # saves memory for longer sequences, prolongs computation a little bit
 
 tokenizer = AutoTokenizer.from_pretrained(name)
-tokenizer.pad_token = tokenizer.eos_token # add pad token
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.add_eos_token = True
+tokenizer.add_bos_token, tokenizer.add_eos_token
+#if tokenizer.pad_token is None:
+#    tokenizer.pad_token = tokenizer.eos_token
+    #tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    #model.resize_token_embeddings(len(tokenizer))
+#tokenizer.add_special_tokens=False
+#tokenizer.pad_token = tokenizer.eos_token # add pad token
+#tokenizer.padding_side = "right"  # Important for consistent behavior
+#tokenizer.add_eos_token = True 
 
 #model.resize_token_embeddings(len(tokenizer)) # edit model size according to the new tokenizer size
 
@@ -155,9 +165,16 @@ trained_model_save_path = SERVER_PTH + "/lora/knn_models/" + args.modelSavePth #
 
 """Prepare the dataset"""
 # load dataset from the HuggingFace Hub
-dataset = load_dataset("Nirmata/Movie_evaluation")
-path_validation = SERVER_PTH + '/lora/dataset/validation.json'
-path_train = SERVER_PTH + '/lora/dataset/train.json'
+#dataset = load_dataset("Nirmata/Movie_evaluation")
+
+# object dataset paths
+train_dataset_path = "/storage/brno2/home/martom198/lora/dataset/train_dataset.pkl"
+validation_dataset_path = "/storage/brno2/home/martom198/lora/dataset/valid_dataset.pkl"
+test_dataset_path = "/storage/brno2/home/martom198/lora/dataset/test_dataset.pkl"
+
+path_validation = SERVER_PTH + '/lora/dataset/validation_trimmed.json'
+path_train = SERVER_PTH + '/lora/dataset/train_trimmed.json'
+path_test = SERVER_PTH + '/lora/dataset/test_trimmed.json'
 
 # set the splits
 data_files_validation = {
@@ -166,14 +183,37 @@ data_files_validation = {
 data_files_train = {
     "train": path_train,
 }
+data_files_test = {
+    "test": path_test,
+}
 
 # actually load
 
 # get training dataset
-train_dataset = load_dataset("json", data_files=data_files_train)["train"]
+train_dataset = load_dataset("json", data_files=data_files_train, streaming=True)["train"]
 # get validation dataset
-valid_dataset = load_dataset("json", data_files=data_files_validation)["validation"]
+valid_dataset = load_dataset("json", data_files=data_files_validation, streaming=True)["validation"]
+# get test dataset
+# test_dataset = load_dataset("json", data_files=data_files_test)["test"]
+"""
+# store the train_dataset object to a file
+with open(train_dataset_path, "wb") as f:
+    pickle.dump(train_dataset, f)
 
+# store the validation_dataset object to a file
+with open(validation_dataset_path, "wb") as f:
+    pickle.dump(valid_dataset, f)
+
+# store the test_dataset object to a file
+with open(test_dataset_path, "wb") as f:
+    pickle.dump(test_dataset, f)
+
+# load stored dataset
+with open(train_dataset_path, "rb") as f:
+    train_dataset = pickle.load(f)
+with open(validation_dataset_path, "rb") as f:
+    valid_dataset = pickle.load(f)
+"""
 # prepare LoRA configuration
 peft_lora_config = LoraConfig(
     r=args.r,
@@ -191,9 +231,9 @@ peft_lora_config = LoraConfig(
     #target_modules=["q_proj", "v_proj"],  # modules to adapt
 )
 
-lora_alpha = 32 #16
+lora_alpha = 8 #16
 lora_dropout = 0.05 #0.1
-lora_rank = 32 #64
+lora_rank = 8 #64
 
 print(f"before: {sum(params.numel() for params in model.parameters() if params.requires_grad)}")
 model = get_peft_model(model, peft_lora_config)
@@ -202,8 +242,8 @@ print(f"after: {sum(params.numel() for params in model.parameters() if params.re
 # Create data collator for language modeling
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
-steps_per_epoch = int(len(train_dataset) / TRAIN_BATCH_SIZE)
-eval_steps_count = round(steps_per_epoch / 3)
+steps_per_epoch = 10 #int(len(train_dataset) / TRAIN_BATCH_SIZE)
+eval_steps_count = 3000 #round(steps_per_epoch / 3)
 
 """
 the training arguments are inspired by: https://www.mldive.com/p/how-to-fine-tune-llama-2-with-lora
@@ -227,27 +267,47 @@ training_args = TrainingArguments(
   eval_strategy ='steps', # to evaluate every EVAL_STEPS_COUNT
   eval_steps=eval_steps_count,
   save_strategy='steps',
+  max_steps=10000,
   load_best_model_at_end=True,
   metric_for_best_model='loss',
   greater_is_better=False,
+  dataset_text_field="chat_sample",
   seed=SEED_TRAIN
   #max_length=1024
 )
 
-try:
+def create_prompt(sample):
+    bos_token = "<s>"
+    system_message = 'Provide information about the movie in following format: {"rating": <rating>, "genres": <genres>, "review": <review_text>}.'
+    eos_token = "</s>"
+
+    full_prompt = ""
+    full_prompt += bos_token
+    full_prompt += "### Instruction:"
+    full_prompt += "\n" + system_message
+    full_prompt += "\n\n### Input:"
+    full_prompt += "\n" + sample["prompt"]
+    full_prompt += "\n\n### Response:"
+    full_prompt += "\n" + sample["completion"]
+    full_prompt += eos_token
+
+    return full_prompt
+
+#try:
     # Train the model
-    trainer = SFTTrainer(
+trainer = SFTTrainer(
     model=model,
     args=training_args,
     data_collator=data_collator,
     train_dataset=train_dataset,
     eval_dataset=valid_dataset,
+    #formatting_func=create_prompt,
     #tokenizer=tokenizer,
     peft_config=peft_lora_config,
     #dataset_text_field="text" # dictates dataset formatting
-    )
-except Exception as e:
-    print(str(e))
+)
+#except Exception as e:
+#    print(str(e))
 
 trainer.train() # run fine-tuning
 trainer.save_model(trained_model_save_path) #save custom model
